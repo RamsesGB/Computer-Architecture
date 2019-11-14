d528638c64ca3ca208309c8ffea680604d4ff04b
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-11-14 17:07:06 -0500

    working through day 3 MVP

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,63 +3,66 @@
 import sys
 
 
-
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256 # allocate 256 bytes of memory
-        self.pc = 0
+        self.branch_table = {}
+        self.branch_table[0b10000010] = {
+            "instruction": "LDI", "handle": self.handle_LDI}
+        self.branch_table[0b01000111] = {
+            "instruction": "PRN", "handle": self.handle_PRN}
+        self.branch_table[0b10100010] = {
+            "instruction": "MUL", "handle": self.handle_ALU}
+        self.branch_table[0b01000101] = {
+            "instruction": "PUSH", "handle": self.handle_PUSH}
+        self.branch_table[0b01000110] = {
+            "instruction": "POP", "handle": self.handle_POP}
+        self.branch_table[0b00000001] = {
+            "instruction": "HLT", "handle": self.handle_HLT}
+        self.ram = [0] * 256
         self.reg = [0] * 8
-        self.fl = 0
-        self.operations = {
-            "LDI": 0b10000010,
-            "HLT": 0b00000001,
-            "PRN": 0b01000111,
-            "ADD": 0b10100000,
-            "MUL": 0b10100010,
-            "PUSH": 0b01000101,
-            "POP": 0b01000110,
-         }
-
-    def ram_read(self, MAR):
-        value = self.ram[MAR]
-        return value
-
-    def ram_write(self, address, MDR):
-        self.ram[address] = MDR
+        self.pc = 0
+        self.sp = 7
+        self.reg[self.sp] = 0xf4
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print("USAGE: ls8.py filename")
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        filename = sys.argv[1]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(filename) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()  # remove spaces
 
+                if line == "":
+                    continue
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+                val = int(line, 2)
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
+                self.ram[address] = val
+
+                address += 1
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self, pc, value):
+        self.ram[pc] = value
+
+    def reg_read(self, pc):
+        return self.reg[pc]
+
+    def reg_write(self, pc, value):
+        self.reg[pc] = value
 
     def trace(self):
         """
@@ -69,38 +72,75 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %i" % self.reg[i], end='')
 
         print()
 
-    def run(self):
-        """Run the CPU."""
-        halted = False
+    def handle_LDI(self, op):
+        register = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
 
-        while not halted:
-            IR = self.ram[self.pc]
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
+        self.reg_write(register, value)
 
-        if IR == self.operations["LDI"]:
-            self.reg[operand_a] = operand_b
-            self.pc += 3
+        self.pc += 3
+        self.trace()
 
-        elif IR == self.operations["PRN"]:
-            print(self.reg[operand_a])
-            self.pc += 2
+    def handle_ALU(self, op):
+        """ALU operations."""
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
 
-        elif IR == self.operations["HLT"]:
-            halted = True
-
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            multiplication = self.reg_read(reg_a) * self.reg_read(reg_b)
+            self.reg_write(reg_a, multiplication)
         else:
-            print(f"Unknown instruction at index {self.pc}")
-            sys.exit(1)
+            raise Exception("Unsupported ALU operation")
+
+        self.pc += 3
+        self.trace()
+
+    def handle_PUSH(self, op):
+        self.reg[self.sp] -= 1
+        reg_location = self.ram[self.pc + 1]
+        reg_value = self.reg[reg_location]
+
+        self.ram[self.reg[self.sp]] = reg_value
+
+        self.pc += 2
+
+    def handle_POP(self, op):
+        value = self.ram[self.reg[self.sp]]
+        reg_location = self.ram[self.pc + 1]
+        self.reg[reg_location] = value
+
+        self.reg[self.sp] += 1
+
+        self.pc += 2
+
+    def handle_PRN(self, op):
+        register_value = self.reg_read(self.ram_read(self.pc + 1))
+
+        print(f"PRN: R{self.ram_read(self.pc + 1)}: {register_value}")
+
+        self.pc += 2
+        self.trace()
+
+    def handle_HLT(self, op):
+        print("HLT")
+        sys.exit(1)
+
+    def run(self):
+        while True:
+            instruction = self.ram_read(self.pc)
+            retrieve = self.branch_table[instruction]
+            retrieve["handle"](retrieve["instruction"])
